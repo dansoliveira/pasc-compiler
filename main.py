eaf48d117ca67a183f1ecdf328df85a40f19ef2b
Daniel Santos de Oliveira <dansoliveira@outlook.com>
2018-06-10 23:56:03 -0300

    Correções no main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from lexer import Lexer
 from parser import Parser
 
 if __name__ == "__main__":
-    lexer = Lexer("teste2.pasc")
+    lexer = Lexer("exemplos/teste2.pasc")
     parser = Parser(lexer)
 
     parser.executa()
-
-    lexer.tabela_de_simbolos.imprime_ts()
